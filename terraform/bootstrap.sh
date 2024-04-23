5d7413862340535014e5ef76acc4398738d17f49
PavanKalyan49r <pavan49r@gmail.com>
2024-04-23 11:22:46 +0530

    jenkins

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -4,4 +4,4 @@ environment=$2 #dont use env here, it is resevered in linux
 app_version=$3
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
-ansible-pull -U https://github.com/PavanKalyan49r/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
+ansible-pull -U https://github.com/PavanKalyan49r/rart.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
